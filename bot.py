07495f5c51e9cef7678b1f8537c6c40b0f76accd
Danil Savelev <danil14569@mail.ru>
2025-03-12 13:49:40 +0300

    Сообщения бота сменяют друг друга; обработчие повтороного нажатия /start

--- bot.py
+++ bot.py
@@ -20,6 +20,9 @@ def get_db_connection():
 async def send_welcome(message: Message):
     global last_message_id, photo_url
 
+    if last_message_id is not None:
+        await bot.delete_message(chat_id=message.chat.id, message_id=last_message_id)
+
     # Добавление пользователя в базу данных
     user_id = message.from_user.id
     username = message.from_user.username
@@ -33,20 +36,35 @@ async def send_welcome(message: Message):
     INSERT OR IGNORE INTO users (user_id, username, first_name, last_name)
     VALUES (?, ?, ?, ?)
     ''', (user_id, username, first_name, last_name))
+    sub_status = cursor.execute('SELECT is_subscribed FROM users WHERE user_id = ?', (message.from_user.id,)).fetchone()
+    link = cursor.execute('SELECT link FROM users WHERE user_id = ?', (message.from_user.id,)).fetchone()
     conn.commit()
-    conn.close()
 
-    gift_btn = InlineKeyboardButton(text="Получить подарок", callback_data="gift_btn")
-    inline_kbd = InlineKeyboardMarkup(inline_keyboard=[[gift_btn]])
+    # Обработка случая повтороного вызова start с полученным подарком
+    if sub_status[0]:
+        support_btn = InlineKeyboardButton(text="Техподдержка", callback_data="support_btn")
+        app_btn = InlineKeyboardButton(text="Открыть приложение", web_app=WebAppInfo(url=link[0]))
 
-    msg = await message.answer_photo(photo=photo_url,
-                               reply_markup=inline_kbd,
-                               caption="Привет! Этот бот поможет тебе найти "
-                                       "близжайшую свободную автомойку. Специально для есть подарок,"
-                                       " заберите его прямо сейчас!!!"
-                               )
-    
+        inline_kbd = InlineKeyboardMarkup(inline_keyboard=[[support_btn], [app_btn]])
+
+        msg = await message.answer_photo(
+            photo=photo_url,
+            reply_markup=inline_kbd,
+            caption="👋Привет!\n\n"
+            "Этот бот поможет тебе найти ближайшую свободную автомойку."
+        )
+    else: # Вызывается до получения подарка
+        gift_btn = InlineKeyboardButton(text="Получить подарок", callback_data="gift_btn")
+        inline_kbd = InlineKeyboardMarkup(inline_keyboard=[[gift_btn]])
+
+        msg = await message.answer_photo(photo=photo_url,
+                                reply_markup=inline_kbd,
+                                caption="👋Привет!\n\n"
+                                "Этот бот поможет тебе найти ближайшую свободную автомойку.\n\n"
+                                "🎁Месяц бесплатной подписки для новых клиентов"
+                                )    
     last_message_id = msg.message_id
+    conn.close()
     
 
 @dp.callback_query(F.data == "gift_btn")
@@ -69,16 +87,32 @@ async def gift_cmd(callback: CallbackQuery):
     conn.close()
 
 
-    app_btn = InlineKeyboardButton(text="Открыть приложение", web_app=WebAppInfo(url=link[0]))
+    app_btn = InlineKeyboardButton(text="Открыть приложение", web_app=WebAppInfo(url=link[0]), callback_data="web_app_btn")
     support_btn = InlineKeyboardButton(text="Техподдержка", callback_data="support_btn")
     kbd = InlineKeyboardMarkup(inline_keyboard=[[support_btn], [app_btn]])
 
 
-    await callback.message.answer_photo(
-        caption="Поздравляем, вы получили месяц бесплатной подписки",
+    msg = await callback.message.answer_photo(
+        caption="🥳Поздравляем!!!\n\n Вы получили месяц бесплатной подписки",
         reply_markup=kbd,
         photo=photo_url
-        )    
+        )   
+    last_message_id = msg.message_id 
+
+
+@dp.callback_query(F.data == "support_btn")
+async def gift_cmd(callback: CallbackQuery):
+    global photo_url, last_message_id
+
+    await bot.delete_message(chat_id=callback.message.chat.id, message_id=last_message_id)
+    await callback.answer()
+
+    msg = await callback.message.answer_photo(
+        photo=photo_url,
+        caption="Если у вас возникли проблемы, то вы можете написать на наш официальный аккаунт\n\n@danilxl"
+    )
+    last_message_id = msg.message_id 
+
 
 
 if __name__ == '__main__':
